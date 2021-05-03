0a971f0fab9e7c51b4306d3bcf551ae80378517a
Max Lockwood <59417148+Max88-git@users.noreply.github.com>
2021-05-03 09:45:10 +0100

    Added ContactManager component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './style.css';
 
 // AddPersonForm: a form with the text field and Add button
-//AddPersonForm uses state to manage the value of the text field
+// AddPersonForm uses state to manage the value of the text field
 function AddPersonForm() {
 	const [ person, setPerson ] = useState('');
 
@@ -33,13 +33,23 @@ function PeopleList(props) {
 	const listItems = arr.map((val, index) => <li key={index}>{val}</li>);
 	return <ul>{listItems}</ul>;
 }
+// ContactManager: Parent component of AddPersonForm and PeopleList and holds the contacts list in its state.
+// ContactManager component receives the initial contacts list using props, saves it in its state.
+// Then it passes down the contacts list to its child component.
+function ContactManager(props) {
+	const [ contacts, setContacts ] = useState(props.data);
+
+	return (
+		<div>
+			<AddPersonForm />
+			<PeopleList data={contacts} />
+		</div>
+	);
+}
 //Render our components on the page and include some initial data
 const contacts = [ 'James Smith', 'Thomas Anderson', 'Bruce Wayne' ];
-const el = (
-	<div>
-		<AddPersonForm />
-		<PeopleList data={contacts} />
-	</div>
-);
 
-ReactDOM.render(el, document.getElementById('root'));
+ReactDOM.render(
+	<ContactManager data={contacts} />,
+	document.getElementById('root')
+);
